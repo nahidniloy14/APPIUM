7569e683321b64a6be2ac8d0378cbae1bd0a5449
Niloy <nahidniloy894@gmail.com>
2022-12-12 10:46:43 +0600

    12/11/22

---
+++ IOS Automation/DesiredCapabilities.py
@@ -0,0 +1,10 @@
+from appium import webdriver
+
+desired_caps = {}
+desired_caps['platformName'] = 'IOS'
+desired_caps['platformVersion'] = '13.2'
+desired_caps['deviceName'] = 'iPhone 11 Pro'
+desired_caps['automationName'] = 'XCUITest'
+desired_caps['app'] = ('/Users/sujithreddy/Documents/IOS_APPS/UICatalog.app')
+
+driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
